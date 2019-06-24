2b2539607ab7c4908a9211ae79d3ddf0afa91f3a
Chris Bush <chris.bush@mongodb.com>
2019-06-24 17:09:46 -0400

    Fix imports

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import Cell from './Cell'
-
 import {dot} from 'mathjs'
-
-const math = require('..')
-const random = require('random')
-
+import random from 'random'
 
 const BOARD_SIZE = 5
-const N1 = [ 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0 ]
-const N2 = [ 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1 ]
-//const zero = zeros(5)
+const N1 = [0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0]
+const N2 = [1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1]
+// const zero = zeros(5)
 
 function indexToXY(index, width) {
   return [
@@ -28,20 +24,20 @@ function checkBoard() {
 
   for (let i = 0; i < BOARD_SIZE * BOARD_SIZE; ++i) {
     board_array.push(random.int(0, 1))
-  } 
-  //const board_matrix = reshape(board_array, [BOARD_SIZE*BOARD_SIZE, 1])
+  }
+  // const board_matrix = reshape(board_array, [BOARD_SIZE*BOARD_SIZE, 1])
 
   const d1 = dot(board_array, N1)
-  const d2 = dot(board_array, N2) 
+  const d2 = dot(board_array, N2)
   console.log(d1)
   console.log(d2)
 
-  
-  if (d1 == 8 && d2== 6) {
+
+  if (d1 == 8 && d2 == 6) {
     return board_array
-  } else {
-    checkBoard()
   }
+  checkBoard()
+
 
   return board_array
 }
@@ -49,13 +45,13 @@ function checkBoard() {
 export default class Board extends React.Component {
   constructor(props) {
     super(props)
-    const board = [] 
+    const board = []
     const board_arr = checkBoard()
 
-    for (let i = 0; i < BOARD_SIZE * BOARD_SIZE; ++i) { 
-      if (board_arr[i] == 1){
+    for (let i = 0; i < BOARD_SIZE * BOARD_SIZE; ++i) {
+      if (board_arr[i] == 1) {
         board.push({
-          active: true, 
+          active: true,
         })
       } else {
         board.push({
